937e55a68f2db76b11be75ed0242328cdead09d2
Victor Vazquez <vhvb1989@gmail.com>
2020-04-03 22:56:09 -0700

    encoder

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace puchuCryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdTransform = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdTransform
+            // 
+            this.cmdTransform.Location = new System.Drawing.Point(1145, 718);
+            this.cmdTransform.Name = "cmdTransform";
+            this.cmdTransform.Size = new System.Drawing.Size(247, 86);
+            this.cmdTransform.TabIndex = 0;
+            this.cmdTransform.Text = "&transform";
+            this.cmdTransform.UseVisualStyleBackColor = true;
+            this.cmdTransform.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(67, 451);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(1018, 353);
+            this.txtResult.TabIndex = 1;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(67, 58);
+            this.txtCode.Multiline = true;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(1018, 353);
+            this.txtCode.TabIndex = 2;
+            this.txtCode.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1441, 897);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.cmdTransform);
+            this.Name = "Form1";
+            this.Text = "Puchu Cryptor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdTransform;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PuchuCode.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace puchuCryptor
+{
+    public class PuchuCode
+    {
+        private static char[,] code = {
+            {'B','T','W','R','5','2','D','I','A','Y','F','W','H','E','J','V'},
+            {'S','H','M','A','V','P','E','P','Y','D','E','Z','W','T','E','V'},
+            {'V','1','C','A','N','8','N','M','O','T','C','X','Y','L','S','C'},
+            {'A','8','N','N','I','U','V','E','R','D','S','A','U','A','R','Y'},
+            {'W','A','F','I','T','6','Q','N','L','S','L','H','G','Q','S','D'},
+            {'D','2','G','T','H','E','R','<','3','G','B','R','V','O','W','A'},
+            {'O','7','L','D','D','W','I','Q','9','O','F','T','H','V','I','U'},
+            {'Z','X','C','M','Y','S','X','L','X','I','M','E','F','X','E','E'},
+        };
+        public PuchuCode()
+        {
+        }
+
+        public String encode(String raw)
+        {
+            return "WIP";
+        }
+
+        public String decode(String cryp)
+        {
+            String result = "";
+            // lines
+            foreach (string line in cryp.Split('\n'))
+            {
+                // tokens
+                foreach (string token in line.Split(' '))
+                {
+                    if (token.Length > 2)
+                    {
+                        // letter
+                        string[] c = token.Split(',');
+                        int r = int.Parse(c[0].Trim().Replace('c', '0')) - 1;
+                        int col = int.Parse(c[1].Trim().Replace('r', '0')) -1;
+                        result += code[col, r];
+                    }
+                    else
+                    {
+                        // expected space
+                        result += " ";
+                    }
+                }
+
+                // add break
+                result += Environment.NewLine;
+            }
+
+            return result;
+        }
+    }
+}
+
